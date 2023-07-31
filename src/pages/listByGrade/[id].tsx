43b297f0f855a30b5c2cd8cd58f67616875614f1
yurixtugal <yurixtugal12@gmail.com>
2023-07-31 00:58:26 -0500

    Fix some build errors

--- src/pages/listByGrade/[id].tsx
+++ src/pages/listByGrade/[id].tsx
@@ -36,7 +36,7 @@ const ListByGrades = ({schoolList}: Props) => {
                     </thead>
                     <tbody>
                       {schoolList.productSchoolLists.map((productList,index)=>(
-                        <tr className="border-b transition duration-300 ease-in-out hover:bg-neutral-100 dark:border-neutral-500 dark:hover:bg-neutral-600">
+                        <tr key={index} className="border-b transition duration-300 ease-in-out hover:bg-neutral-100 dark:border-neutral-500 dark:hover:bg-neutral-600">
                         <td className="whitespace-nowrap px-6 py-4 font-medium">
                           {index+1}
                         </td>
